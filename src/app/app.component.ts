693efd24512413fcba705141dde2ac6cfe035f45
Aleksandr Tolstykh <tolstykh.alex@gmail.com>
2021-10-26 13:17:41 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,95 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce-product-page';
+  mobile: boolean = false;
+  showMenu: boolean = false;
+  showLightbox: boolean = false;
+  showCart: boolean = false;
+  selectedItem: number = 1;
+  addItems: number = 0;
+  totalInCart: number = 0;
+
+  ngOnInit(): void {
+    this.mobile = (window.innerWidth <= 768) ? true : false;
+  }
+
+  onResize(event: any) {
+    this.mobile = (event.target.innerWidth <= 768) ? true : false;
+  }
+
+  onClick(event: any) {
+    this.showMenu = !this.showMenu;
+  }
+
+  onOpenLightbox(event: any) {
+    this.showLightbox = true;
+    document.getElementById("lightbox").style.backgroundColor = "hsl(0, 0%, 0%, 75%)";
+    document.getElementById("lightbox").style.zIndex = "0";
+  }
+
+  onCloseLightbox(event: any) {
+    this.showLightbox = false;
+    document.getElementById("lightbox").style.backgroundColor = "transparent";
+    document.getElementById("lightbox").style.zIndex = "-1";
+  }
+
+  addItem(event: any) {
+    this.addItems++;
+  }
+
+  removeItem(event: any) {
+    if (this.addItems !== 0) {
+      this.addItems--;
+    }
+  }
+
+  addToCart(event: any) {
+    this.totalInCart += this.addItems;
+    this.addItems = 0;
+  }
+
+  selectItem1(event: any) {
+    this.selectedItem = 1;
+  }
+
+  selectItem2(event: any) {
+    this.selectedItem = 2;
+  }
+
+  selectItem3(event: any) {
+    this.selectedItem = 3;
+  }
+
+  selectItem4(event: any) {
+    this.selectedItem = 4;
+  }
+
+  moveBack(event: any) {
+    if (this.selectedItem === 1) {
+      this.selectedItem = 4;
+    } else {
+      this.selectedItem--;
+    }
+  }
+
+  moveForward(event: any) {
+    if (this.selectedItem === 4) {
+      this.selectedItem = 1;
+    } else {
+      this.selectedItem++;
+    }
+  }
+
+  onCartClick(event: any) {
+    this.showCart = !this.showCart;
+  }
+
+  onRemoveFromCart(event: any) {
+    this.totalInCart = 0;
+  }
+
+  getTotalPrice(): string {
+    let price = 125 * this.totalInCart;
+    return "$" + price + ".00";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
